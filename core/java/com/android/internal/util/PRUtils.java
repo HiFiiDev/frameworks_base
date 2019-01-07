751b96134aaf3f7e96de50f4c5da4dbaaf76560e
Alex Cruz <du.alexcruz@gmail.com>
2019-01-06 20:00:43 -0600

    Battery saver: Ensure warning doesn't pop up while plugged in
    This doesn't make any sense because if you're plugged in then you don't
    need the battery saver.
    
    - Introduced method to determine if plugged in or not, works with wireless
    charging as well.
    
    Steps to duplicate fail:
    - Clean flash or make sure you've never turned on battery saver
    - Plug in your phone
    - Pull down QS panel
    - Tap on the battery QS tile
    
    Change-Id: Idd9f5382ac68b5af0cf8e3d65c670c2982b62a54
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- core/java/com/android/internal/util/PRUtils.java
+++ core/java/com/android/internal/util/PRUtils.java
@@ -17,7 +17,10 @@
 package com.android.internal.util;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.BatteryManager;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -59,5 +62,17 @@ public class PRUtils {
                 // do nothing.
             }
         }
+    }
+
+    // Method to detect if device is plugged in (wired or wireless)
+    public static boolean isPlugged(Context context) {
+        boolean isPlugged;
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        isPlugged = plugged ==
+                BatteryManager.BATTERY_PLUGGED_AC || plugged == BatteryManager.BATTERY_PLUGGED_USB;
+        isPlugged = isPlugged || plugged == BatteryManager.BATTERY_PLUGGED_WIRELESS;
+        return isPlugged;
     }
 }
